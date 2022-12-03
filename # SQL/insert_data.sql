e4ac7a6416cf23d4a611947013e9e7d21c510afc
솜놜 <90139018+somnwal@users.noreply.github.com>
2022-12-04 07:49:30 +0900

    UPDATE SQL

--- # SQL/insert_data.sql
+++ # SQL/insert_data.sql
@@ -1,56 +1,56 @@
 USE `costudy`;
 
-INSERT INTO USER
+INSERT INTO `user`
 (`user_id`,
 `user_password`,
 `user_nick`,
 `user_email`,
 `user_born`)
 VALUES
-("asdf",
+("yeongha_kim",
 "1234",
-"테스트",
-"admin@admin.com",
+"김영하",
+"kim_yeong_ha@costudy.com",
 "1996-02-01");
 
-INSERT INTO USER
+INSERT INTO `user`
 (`user_id`,
 `user_password`,
 `user_nick`,
 `user_email`,
 `user_born`)
 VALUES
-("aaaa",
+("cjw1234",
 "1234",
-"테스트2",
-"admin@admin.com",
-"1996-02-01");
+"최진우",
+"cjw1234@costudy.com",
+"2000-08-16");
 
-INSERT INTO USER
+INSERT INTO `user`
 (`user_id`,
 `user_password`,
 `user_nick`,
 `user_email`,
 `user_born`)
 VALUES
-("bbbb",
+("leems4669",
 "1234",
-"테스트3",
-"admin@admin.com",
-"1996-02-01");
+"이민수",
+"leems4669@costudy.com",
+"1984-12-27");
 
-INSERT INTO USER
+INSERT INTO `user`
 (`user_id`,
 `user_password`,
 `user_nick`,
 `user_email`,
 `user_born`)
 VALUES
-("cccc",
+("jangyy",
 "1234",
-"테스트4",
-"admin@admin.com",
-"1996-02-01");
+"장윤영",
+"jangyy@costudy.com",
+"1995-01-15");
 
 INSERT INTO `costudy`.`study_group`
 (`group_id`,
@@ -62,28 +62,11 @@ INSERT INTO `costudy`.`study_group`
 `group_mem_count`)
 VALUES
 (0,
-"aaaa",
-"test1",
+"jangyy",
+"같이 C언어 공부하실 분!",
 "8:00",
-"12:00",
+"10:00",
 "37.5650172,126.8494668",
-2);
-
-INSERT INTO `costudy`.`study_group`
-(`group_id`,
-`group_master`,
-`study_name`,
-`study_start`,
-`study_finish`,
-`study_location`,
-`group_mem_count`)
-VALUES
-(0,
-"aaaa",
-"test2",
-"8:00",
-"12:00",
-"37.6010077,239.2010199",
 1);
 
 INSERT INTO `costudy`.`study_group`
@@ -96,10 +79,27 @@ INSERT INTO `costudy`.`study_group`
 `group_mem_count`)
 VALUES
 (0,
-"asdf",
-"test3",
-"8:00",
-"12:00",
+"yeongha_kim",
+"자바 강의 같이 들을사람만",
+"16:00",
+"18:00",
+"37.6010077,239.2010199",
+3);
+
+INSERT INTO `costudy`.`study_group`
+(`group_id`,
+`group_master`,
+`study_name`,
+`study_start`,
+`study_finish`,
+`study_location`,
+`group_mem_count`)
+VALUES
+(0,
+"leems4669",
+"스프링 스터디 모집합니다",
+"06:00",
+"23:00",
 "37.4368585,126.8726502",
 1);
 
@@ -110,7 +110,7 @@ INSERT INTO `costudy`.`group_reg`
 `reg_state`)
 VALUES
 (0,
-"aaaa",
+"jangyy",
 1,
 "2");
 
@@ -121,7 +121,7 @@ INSERT INTO `costudy`.`group_reg`
 `reg_state`)
 VALUES
 (0,
-"aaaa",
+"yeongha_kim",
 2,
 "2");
 
@@ -132,30 +132,8 @@ INSERT INTO `costudy`.`group_reg`
 `reg_state`)
 VALUES
 (0,
-"asdf",
-1,
-"2");
-
-INSERT INTO `costudy`.`group_reg`
-(`reg_id`,
-`user_id`,
-`group_id`,
-`reg_state`)
-VALUES
-(0,
-"asdf",
+"cjw1234",
 2,
-"1");
-
-INSERT INTO `costudy`.`group_reg`
-(`reg_id`,
-`user_id`,
-`group_id`,
-`reg_state`)
-VALUES
-(0,
-"asdf",
-3,
 "2");
 
 INSERT INTO `costudy`.`group_reg`
@@ -165,9 +143,9 @@ INSERT INTO `costudy`.`group_reg`
 `reg_state`)
 VALUES
 (0,
-"bbbb",
-3,
-"1");
+"leems4669",
+2,
+"2");
 
 INSERT INTO `costudy`.`group_reg`
 (`reg_id`,
@@ -176,6 +154,8 @@ INSERT INTO `costudy`.`group_reg`
 `reg_state`)
 VALUES
 (0,
-"cccc",
+"leems4669",
 3,
-"1");
+"2");
+
+COMMIT;
